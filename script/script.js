b9c9a456668a466dbe8452f25a6a601e2b42fced
Teo <seminara.teodoro93@gmail.com>
2025-09-15 18:27:00 +0200

    refactoring

--- script/script.js
+++ script/script.js
@@ -1,5 +1,16 @@
 const generateEmail = "https://flynn.boolean.careers/exercises/api/random/mail";
 const insertEmail = document.getElementById("mail-generate");
+const btnGenerate = document.getElementById("btn-generate")
+
+randomMail();
+
+btnGenerate.addEventListener("click",
+    function () {
+        randomMail();
+        insertEmail.innerHTML = "";
+
+    }
+);
 
 
 
@@ -7,12 +18,8 @@ function randomMail() {
     for (let i = 0; i < 10; i++) {
         axios.get(generateEmail).then(response => {
             // let result = response.data.response;
-            insertEmail.innerHTML += `<li>${response.data.response}</li>`;
+            insertEmail.innerHTML += `<li class="list-group-item">${response.data.response}</li>`;
         })
     }
 
 };
-
-
-
-randomMail();
