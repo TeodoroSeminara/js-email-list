c8a3192481f7939ca59179a33e71c519b61a92e7
Teo <seminara.teodoro93@gmail.com>
2025-09-15 17:26:13 +0200

    esercizio completo

--- script/script.js
+++ script/script.js
@@ -1,30 +1,18 @@
 const generateEmail = "https://flynn.boolean.careers/exercises/api/random/mail";
 const insertEmail = document.getElementById("mail-generate");
 
-for (let i = 0; i < 10; i++) {
-    let newMail = randomMail();
-    insertEmail.innerHTML = `<li>${insertEmail}</li>`;
-    console.log("questa è la mail", newMail);
-
-
-
-}
-
-
 
 
 function randomMail() {
-    axios.get(generateEmail)
-        .then(rispApi => {
-
-            const result = rispApi.data.response;
-            console.log(result);
-            // insertEmail.appendChild(document.createElement("li"));
-            // const [emailGenerated] = result;
-
+    for (let i = 0; i < 10; i++) {
+        axios.get(generateEmail).then(response => {
+            // let result = response.data.response;
+            insertEmail.innerHTML += `<li>${response.data.response}</li>`;
         })
-        .catch(error => {
-            // codice da eseguire in caso di errore
-            console.error(error)
-        });
-}
+    }
+
+};
+
+
+
+randomMail();
