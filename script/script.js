fa06c43ece584ffc28e4e0f6efd4efbe20209c59
Teo <seminara.teodoro93@gmail.com>
2025-09-15 15:08:24 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const generateEmail = "https://flynn.boolean.careers/exercises/api/random/mail";
+const insertEmail = document.getElementById("mail-generate");
+
+for (let i = 0; i < 10; i++) {
+
+
+}
+
+
+
